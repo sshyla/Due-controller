52b39cb1aabcf07c1df9c899ed581527d5fa2494
Morbo <dudlsack@gmail.com>
2016-03-18 00:41:55 +0100

    Added endstop, and changes

--- edge_controller.ino
+++ edge_controller.ino
@@ -1,13 +1,24 @@
+
+
 int pinCount = 8;
-int motorAxisX[8] = {7, 6, 2, 3, 10, 9, 11, 12};
+int motorAxisX[9] = {7, 6, 2, 3, 10, 9, 11, 12, 777};
 int timer = 1500;
+long schrittAnzahl = 0;
+int taktAnzahl = 500; // Anzahl von schritten pro umdrehung
+int leadScrewPitch = 0.5; // mm/umdrehung
+//int linearBewegung = stepp / pitch // takt/mmm (linear)
+long motorStepCount[3] = {0};
+
+
 /*====================================================================================*/
 void setup() {
- // SerialUSB.begin(9600);
+  /*=====*/
+  attachInterrupt(4,interrupt5,CHANGE);
+  /*=====*/
+  
+  // SerialUSB.begin(9600);
   for (int i = 4; i < pinCount; i++) {
     pinMode(motorAxisX[i], OUTPUT);
-  }
-  for (int i = 4; i < pinCount; i++) {
     digitalWrite(motorAxisX[i], LOW);
   }
   for (int i = 0; i < 4; i++) {
@@ -16,11 +27,25 @@ void setup() {
 }
 /*====================================================================================*/
 void loop() {
-  movement(scannerMatrix(motorAxisX));
- // SerialUSB.print(scannerMatrix(motorAxisX));
+  movement(scannerMatrix(motorAxisX), motorAxisX);
+  //  SerialUSB.print();
+}
+/*====================================================================================*/
+//INTERRUPT FUNKTIONEN
+void interrupt5(){
+SerialUSB.println("Interrupt!");
+endStop(motorAxisX);
 }
 /*====================================================================================*/
 //FUNKTIONEN
+int count(int motorArray[]) {
+  // zahlt wie oft ein knopf gedruckt wird, speichert
+}
+void SerialOutput() {
+  // Gibt die momentan schritt anzahl pro motor.
+}
+/*============================================================================================================================================================*/
+
 int scannerMatrix(int motor[]) {
   //scans for active buttons
   for (int i = 0; i < 4; i++) {
@@ -30,13 +55,7 @@ int scannerMatrix(int motor[]) {
   }
   return 999;
 }
-void count() {
-  // zahlt wie oft ein knopf gedruckt wird, speichert
-}
-void SerialOutput() {
-  // Gibt die momentan schritt anzahl pro motor.
-}
-void movement(int scanner) {
+void movement(int scanner, int motor[]) {
   //wahl nach button von scanner welche bewegung funktion
   if (scanner < 4) {
     switch (scanner) {
@@ -44,37 +63,47 @@ void movement(int scanner) {
         do_nothing();
         break;
       case 0:
-        umdrehungPos(motorAxisX);
+        umdrehungPos(motor);
         break;
       case 1:
-        umdrehungNeg(motorAxisX);
+        umdrehungNeg(motor);
         break;
       case 2:
-        einSchrittPos(motorAxisX);
+        einSchrittPos(motor);
         break;
       case 3:
-        einSchrittNeg(motorAxisX);
+        einSchrittNeg(motor);
         break;
     }
   }
 }
 void umdrehungPos(int motor[]) {
   // motor dreht 1 umdrehung in Postive achse richtung
-  digitalWrite(motor[4], HIGH);
-  digitalWrite(motor[5], HIGH);
-  digitalWrite(motor[6], HIGH);
+  digitalWrite(motor[4], LOW);
+  digitalWrite(motor[5], LOW);
+  digitalWrite(motor[6], LOW);
   digitalWrite(motor[7], HIGH);
+  int x = 0;
   delay(timer);
-  for (int i = 4; i < pinCount; i++) {
-    digitalWrite(motorAxisX[i], LOW);
+/*  for (int i = 4; i < pinCount; i++) {
+    digitalWrite(motor, LOW);
   }
+  switch (mark) {
+      case 777:
+      x = motorStepCount[3] + 500;
+      motorStepCount[3] = x;
+      break;
+      case 999:
+      do_nothing();
+      break;
+  } */
 }
 void umdrehungNeg(int motor[]) {
   // motor dreht 1 umdrehung in Negative achse richtung
   digitalWrite(motor[4], HIGH);
   digitalWrite(motor[5], LOW);
   digitalWrite(motor[6], LOW);
-  digitalWrite(motor[7], HIGH);
+  digitalWrite(motor[7], LOW);
   delay(timer);
   for (int i = 4; i < pinCount; i++) {
     digitalWrite(motorAxisX[i], LOW);
@@ -98,6 +127,16 @@ void einSchrittNeg(int motor[]) {
   digitalWrite(motor[6], HIGH);
   digitalWrite(motor[7], HIGH);
   delay(timer);
+  for (int i = 4; i < pinCount; i++) {
+    digitalWrite(motorAxisX[i], LOW);
+  }
+}
+void endStop(int motor[]) {
+  // motor dreht 1 SCHRITT in Negative achse richtung
+  digitalWrite(motor[4], HIGH);
+  digitalWrite(motor[5], HIGH);
+  digitalWrite(motor[6], HIGH);
+  digitalWrite(motor[7], HIGH);
   for (int i = 4; i < pinCount; i++) {
     digitalWrite(motorAxisX[i], LOW);
   }
