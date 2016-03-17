7befe5a55c531fd6060c413004e0981130b9a361
Morbo <dudlsack@gmail.com>
2016-03-18 00:09:49 +0100

    Updating new interrupts
    Added interrupt features/endstops per axis. Will immediately kill the signal while giving a burst of all HIGH I/O to signal Mdrive

--- Due_controller.ino
+++ Due_controller.ino
@@ -1,20 +1,21 @@
 // initilize array 8 pins pro motor Erste VIER PINS SIND INPUT.
-int motorAxisX[8] = {7, 6, 2, 3, 10, 9, 11, 12};
-int motorAxisY[8] = {7, 6, 2, 3, 10, 9, 11, 12};
-int motorAxisC[8] = {7, 6, 2, 3, 10, 9, 11, 12};
+int motorAxisX[9] = {0, 7, 6, 2, 3, 10, 9, 11, 4};
+int motorAxisY[9] = {0, 7, 6, 2, 3, 10, 9, 11, 4};
+int motorAxisC[9] = {0, 7, 6, 2, 3, 10, 9, 11, 4};
 int pinCount = 8; // muss immer 2k aus Nat. zahlen.
-int inputPins = 4; //halfte von pinCount
-int timer = 1500 //MOTOR I/O dauer in ms
-/*====================================================================================*/
+int inputPins = 4; //Erste 4 von pinCount
+int timer = 1500; //MOTOR I/O dauer in ms
+            /*====================================================================================*/
 void setup() {
+  attachInterrupt(4, interrupt5, CHANGE); //IDEALEWEISE fuer an strom eingesetzt.
+  attachInterrupt(5, interrupt5, CHANGE); //IDEALEWEISE fuer an strom eingesetzt.
+  attachInterrupt(6, interrupt5, CHANGE); //IDEALEWEISE fuer an strom eingesetzt.
   for (int i = 4; i < pinCount; i++) {
     //pins 4-7 als Out einstellen
     pinMode(motorAxisX[i], OUTPUT);
     pinMode(motorAxisY[i], OUTPUT);
     pinMode(motorAxisC[i], OUTPUT);
     digitalWrite(motorAxisX[i], LOW);
-    digitalWrite(motorAxisY[i], LOW);
-    digitalWrite(motorAxisC[i], LOW);
   }
   for (int i = 0; i < 4; i++) {
     //pins 0-3 als IN einstellen
@@ -31,6 +32,20 @@ void loop() {
   movement(scannerMatrix(motorAxisC));
   delay(500); // halb sekunde warten
 }
+//INTERRUPT FUNKTIONEN
+void interrupt4() {
+  //  SerialUSB.println("Danger!");
+  endStop(motorAxisX);
+}
+void interrupt5() {
+  //  SerialUSB.println("Danger!");
+  endStop(motorAxisY);
+}
+void interrupt6() {
+  //  SerialUSB.println("Danger!");
+  endStop(motorAxisC);
+}
+
 /*====================================================================================*/
 //FUNKTIONEN
 int scannerMatrix(int motor[]) {
@@ -40,7 +55,7 @@ int scannerMatrix(int motor[]) {
       return i;
     }
   }
-  return 999; // SERIAL DEBUG 
+  return 999; // SERIAL DEBUG
 }
 void movement(int scanner) {
   //wahl nach button von scanner welche bewegung funktion
@@ -70,21 +85,21 @@ zb HHHL, oder LLHH, usw
 */
 void umdrehungPos(int motor[]) {
   // motor dreht 1 umdrehung in Postive achse richtung
-  digitalWrite(motor[4], HIGH);
-  digitalWrite(motor[5], HIGH);
-  digitalWrite(motor[6], HIGH);
+  digitalWrite(motor[4], LOW);
+  digitalWrite(motor[5], LOW);
+  digitalWrite(motor[6], LOW);
   digitalWrite(motor[7], HIGH);
-  delay(timer); 
+  delay(timer);
   for (int i = 4; i < pinCount; i++) {
     digitalWrite(motorAxisX[i], LOW);
   }
 }
 void umdrehungNeg(int motor[]) {
   // motor dreht 1 umdrehung in Negative achse richtung
-  digitalWrite(motor[4], HIGH);
+  digitalWrite(motor[4], LOW);
   digitalWrite(motor[5], LOW);
-  digitalWrite(motor[6], LOW);
-  digitalWrite(motor[7], HIGH);
+  digitalWrite(motor[6], HIGH);
+  digitalWrite(motor[7], LOW);
   delay(timer);
   for (int i = 4; i < pinCount; i++) {
     digitalWrite(motorAxisX[i], LOW);
@@ -110,6 +125,18 @@ void einSchrittNeg(int motor[]) {
   delay(timer);
   for (int i = 4; i < pinCount; i++) {
     digitalWrite(motorAxisX[i], LOW);
+  }
+}
+void endStop(int motor[]) {
+  // motor dreht 1 SCHRITT in Negative achse richtung
+  digitalWrite(motor[4], HIGH);
+  digitalWrite(motor[5], HIGH);
+  digitalWrite(motor[6], HIGH);
+  digitalWrite(motor[7], HIGH);
+  for (int i = 4; i < pinCount; i++) {
+    digitalWrite(motorAxisX[i], LOW);
+    digitalWrite(motorAxisY[i], LOW);
+    digitalWrite(motorAxisC[i], LOW);
   }
 }
 void do_nothing() {
